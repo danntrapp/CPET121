edc4bc39cc73de2fc9ce84443a80a49f476fc203
Jesuschees <dtrapp@hcrhs.org>
2026-02-24 18:38:40 -0500

    2 dimensional array and rand() work

---
+++ Classwork/Day7/Array3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main(void) {
+    int arr[5][5];
+    int sum[] = {0, 0, 0, 0, 0};
+    int minVal = 0;
+    int maxVal = 98;
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j ++) {
+            arr[i][j] = (rand() % (maxVal - minVal + 1)) + minVal; //googled how to get random num within interval
+            cout << arr[i][j] << " ";
+            sum[i] += arr[i][j];
+        }
+        cout << endl;
+    }
+
+    for(int i = 0; i < 4; i++) {
+             cout << "The Sum of ROW["
+         << i << "] :" << right << setw(8) << setfill(' ') << sum[i] << endl;
+        cout << endl;
+    }
+}
