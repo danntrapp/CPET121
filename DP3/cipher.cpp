f25a9c0bc885d0e4f92ea74c76176ec1bc35a72f
Dan Trapp <dtrapp@hcrhs.org>
2026-04-01 20:11:47 -0400

    god only knows what is happening anymore

--- DP3/cipher.cpp
+++ DP3/cipher.cpp
@@ -5,50 +5,125 @@
 #include <iomanip>
 #include <map>
 #include <bitset>
+#include <cstdint>
+#include <filesystem>
 
-std::vector<std::string> split(std::string fullLine) {
-    
-    for(int i = 0; i < fullLine.) {
-        if(!std::isalpha(static_cast<int>(c))) {
-
-        }
-    }
-}
-
-void flipBit(int &bin, int pos) {
-    bin ^= (1 << pos);
-}
-void stringToBin(std::string s, int& bin) {
-    if(s.size() != 5) {
-        std::cout << s << std::endl;
-        throw std::runtime_error("Wrong size dummy");
-    }
-    for(int i = 0; i < 5; i++) {
-        if(std::islower(static_cast<int>(s.at(i)))) {
-            flipBit(bin, i);
-        }
-    }
-}
-std::string decipherBin(const std::vector<int> input) {
-    std::string ret;
-    for(int i : input) {
-        if(i >= 0 && i < 26) {
-            ret.push_back(static_cast<char>(i + 65));
+std::vector<std::string> parseString(std::string fullLine) {
+    std::vector<std::string> ret;
+    std::string s;
+    for(int i = 0; i < fullLine.size(); i++) {
+        if(!std::isalpha(static_cast<int>(fullLine.at(i)))) {
+            fullLine.erase(i);
+        } else {
+            s.append(&fullLine.at(i));
+            if(s.size() == 5) {
+                ret.push_back(s);
+                s.clear();
+            }
         }
     }
     return ret;
 }
+uint8_t stringToBin(std::string s) {
+    uint8_t bin = 0b00000;
+    try {
+        if(s.size() != 5) throw std::invalid_argument("Wrong size dummy");
+
+        auto flipBit = [](uint8_t &bin, int pos) -> void { 
+            bin ^= (1 << pos);
+        };
+
+        for(int i = 0; i < 5; i++) {
+            if(std::islower(static_cast<int>(s.at(i)))) flipBit(bin, i);
+        }
+    } catch(std::invalid_argument &err) {
+        printf(err.what()); 
+    }
+    std::cout << bin << std::endl; //debug
+    return bin;
+}
+std::string decipherBin(const std::vector<uint8_t> input) {
+    const std::map<uint8_t, char> cipherMap = {
+    {0b11111, 'A'},
+    {0b11110, 'B'},
+    {0b11101, 'C'},
+    {0b11100, 'D'},
+    {0b11011, 'E'},
+    {0b11010, 'F'},
+    {0b11001, 'G'},
+    {0b11000, 'H'},
+    {0b10111, 'I'},
+    {0b10110, 'J'},
+    {0b10101, 'K'},
+    {0b10100, 'L'},
+    {0b10011, 'M'},
+    {0b10010, 'N'},
+    {0b10001, 'O'},
+    {0b10000, 'P'},
+    {0b01111, 'Q'},
+    {0b01110, 'R'},
+    {0b01101, 'S'},
+    {0b01100, 'T'},
+    {0b01011, 'U'},
+    {0b01010, 'V'},
+    {0b01001, 'W'},
+    {0b01000, 'X'},
+    {0b00111, 'Y'},
+    {0b00110, 'Z'},
+    {0b00101, '.'},
+    {0b00100, ';'},
+    {0b00011, '!'},
+    {0b00010, '?'},
+    {0b00001, '0'},
+    {0b00000, ' '}};
+
+    std::string ret;
+    for(uint8_t i : input) {
+        try {
+            auto c = cipherMap.find(i);
+            if(c != cipherMap.end()) {
+                ret.push_back(c->second);
+            } else {
+                throw std::runtime_error("Invalid cipher input");
+            }
+        } catch(std::runtime_error& err) {
+            printf("Runtime Error: %s\n", err.what());
+        }
+    }
+    std::cout << ret << std::endl; //debug
+    return ret;
+}
 
 int main() {
-    //     std::string fileName;
-    //     const std::map<int, char> decoderMap = {{0b11111, 'A'},  {0b11110, 'B'}, {0b00010, ''}, }
-    std::vector<int> bins;
+    std::string fileName, line, full;
+    std::ifstream file;
+    std::vector<uint8_t> bins;
+    std::vector<std::string> parsedStrings;
+    // fileName = "humpty.txt";
 
-    std::string input = "HUmPT";
-    bins.resize(1);
-    stringToBin(input, bins.at(0));
-    input = decipherBin(bins);
-    std::cout << input; 
+    // try {
+    //     file.open(fileName.c_str());
+    //     if(!file.is_open()) throw std::runtime_error("File could not be opened");
+    // }   catch(std::runtime_error &err) {
+    //     printf("Runtime Error: %s\n", err.what());
+    //     return 1;
+    // }
 
+    // while(std::getline(file, line)) {
+    //     full.append(line);
+    // }
+    // parsedStrings = parseString(full);
+    // for(std::string s : parsedStrings) {
+    //     bins.push_back(stringToBin(s));
+    // }
+    // full.clear();
+    // full = decipherBin(bins);
+
+    // std::cout << full << std::endl;
+
+    line = "HUmPTY";
+    bins.push_back(stringToBin(line));
+    std::cout << decipherBin(bins);
+    printf("%s", line.c_str());
     return 0;
 }
